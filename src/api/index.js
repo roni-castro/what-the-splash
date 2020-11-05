85b30eeadbfc75eac712f571f552b54b875523a2
Roni <roni.c.castro@gmail.com>
2020-11-04 21:14:11 -0300

    Add fetchImage api service

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+const API_URL = 'https://api.unsplash.com/photos/';
+
+export const fetchImages = async page => {
+    const response = await fetch(
+        `${API_URL}?client_id=${key}&per_page=3&page=${page}`,
+    );
+    const data = await response.json();
+    if (response.status >= 400) {
+        throw new Error(data.errors);
+    }
+    return data;
+};
